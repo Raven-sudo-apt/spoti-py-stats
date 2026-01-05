c6bca1994e84d217b82a99bf5c8f3ab1821d58a3
Raven-sudo-apt <bylasannn@protonmail.com>
2026-01-05 09:30:18 +0200

    first commit

---
+++ Frontend/Spotipy/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AccessToken from './components/accesstoken'
+import GetArtists from './components/getArtists'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
